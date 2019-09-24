21231139fc3b9aabaa866dab2597eb3c471be41b
Joe Badaczewski <joe307bad@gmail.com>
2019-09-24 13:21:11 -0400

    exporting emporium as library

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,9 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        library: 'emporium',
+        libraryTarget: 'commonjs2'
     },
     module: {
         rules: [
