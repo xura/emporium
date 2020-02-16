008ad61b1587dbe4ccd602e93e96dfd649d437d8
Joe Badaczewski <joe307bad@gmail.com>
2020-02-15 23:13:50 -0500

    adding default find options to findAll

--- src/Emporium.ts
+++ src/Emporium.ts
@@ -1,11 +1,11 @@
-import { Repository, Connection, ObjectType } from "typeorm";
+import { Repository, Connection, ObjectType, ObjectLiteral, FindManyOptions } from "typeorm";
 import { inject, autoInjectable, singleton } from "tsyringe";
 import { Observable, of } from 'rxjs';
 import { filter, switchMap, flatMap, map, mergeMap } from 'rxjs/operators';
 import { IRepository } from './interfaces/IRepository';
 
 @autoInjectable()
-export class Emporium<T> implements IRepository<T> {
+export class Emporium<T extends ObjectLiteral> implements IRepository<T> {
     private readonly _connection: () => Connection;
     private readonly _model: ObjectType<T>;
 
@@ -35,11 +35,11 @@ export class Emporium<T> implements IRepository<T> {
                 .save(entity)
                 .then(result => this._entityRepo.save(entity)))
 
-    find = (): Promise<T[]> =>
+    find = (findOptions = { order: { id: 'DESC' } } as FindManyOptions): Promise<T[]> =>
         this._getRepo()
             .then(repo => repo
                 .find()
-                .then((result => this._entityRepo.find())));
+                .then((result => this._entityRepo.find(findOptions))));
 
     stream = (): Promise<Observable<[number, T]>> =>
         this._getRepo().then(repo => repo.stream());
